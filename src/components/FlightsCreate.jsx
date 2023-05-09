69d3fc1edf5797d98a7f22931154f8e68d1b36ee
YX <asdf@hotmail.com>
2023-05-10 07:39:24 +0800

    updated components flightscreate + new component reservations.jsx

--- src/components/FlightsCreate.jsx
+++ src/components/FlightsCreate.jsx
@@ -1,4 +1,29 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const SERVER_URL  = 'http://localhost:3000';
+
 const FlightsCreate = () => {
+    const [flightNumber, setFlightNumber] = useState('');
+    const [destination, setDestination] = useState('');
+    const [date, setDate] = useState('');
+    const [planeId, setPlaneID] = useState('')
+    const [airplanes, setAirplanes] = useState('');
+
+    const fetchAirplanes = () => {
+        axios(SERVER_URL).then((response) => {
+            setAirplanes(response.data);
+        });
+    };
+
+    useEffect(fetchAirplanes, []); 
+
+    const saveAirplanes = (content) => {
+        axios.post(SERVER_URL, {content: content}).then((response) => {
+            setAirplanes([...flights, response.data]);
+        });
+    };
+    
     return (
         <div>
             FlightsCreate cometh
