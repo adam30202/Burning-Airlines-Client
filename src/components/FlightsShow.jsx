517b135ed0826f62bd73c395690f5641d6966586
Adam Shuvalov <adam.shuvalov@gmail.com>
2023-05-11 14:18:12 +1000

    final commit

--- src/components/FlightsShow.jsx
+++ src/components/FlightsShow.jsx
@@ -1,15 +1,33 @@
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
+import axios from 'axios';
+
+const SERVER_URL_RESERVATIONS = 'http://localhost:3000/reservations'
 
 const FlightsShow = (props) => {
     const { flightNumber } = useParams({})
 
     console.log(flightNumber)
     console.log(props.results)
+
+    
+
+    const _submitHandler = (event) => {
+        event.preventDefault();
+        console.log('function')
+
+        axios.post(SERVER_URL_RESERVATIONS, { flight_id: 1, user_id: null,
+        } ).then((response) => {
+          console.log(response.data);
+        });
+    }
+
     return (
         <div className="flight-show-container">
             { props.results[0][0].date } | Flight { props.results[0][0].id } | { props.results[0][0].from } > { props.results[0][0].to }
             <Seats />
+            <p className="seats-left">{ props.results[0][0].seats } Seats Left </p>
+            <ReservationForm reserve={ _submitHandler }/>
         </div>
     );
 };
@@ -19,7 +37,7 @@ const Seats = (props) => {
     const numCols = 4; // Change this to match the number of columns on the airplane
     
     // Create an array of row numbers to use as keys for the seat divs
-    const rows = Array.from(Array(numRows).keys());
+    const rows = Array.from(Array(numRows), (_, i) => i + 1);
     
     // Create an array of column letters to use as labels for the seat divs
     const cols = ['A', 'B', 'C', 'D'].slice(0, numCols);
@@ -29,14 +47,24 @@ const Seats = (props) => {
         {rows.map((row) => (
           <div className="seat-row" key={row}>
             {cols.map((col) => (
-              <div className="seat" key={row + col}>
+              <button className="seat" key={row + col}>
                 {row + col}
-              </div>
+              </button>
             ))}
           </div>
         ))}
       </div>
     );
 };
+
+const ReservationForm = (props) => {
+    return (
+        <div className="reserve-button">
+            <form onSubmit={ props.reserve }>
+                <button id="reserve" >Reserve seat</button>
+            </form>
+        </div>
+    )
+}
 
 export default FlightsShow;
